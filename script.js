102d8c118fa2f52af64e98c3b64dd0c0e62fd9d0
Antioch Sanders <antiochsanders@gmail.com>
2019-03-21 02:33:28 -0400

    Create unused modal for subscriptions

---
+++ script.js
@@ -0,0 +1,11 @@
+/*
+function closeModal() {
+	document.getElementById("sub-backdrop").style.display = "none";
+}
+
+window.onload = function() {
+	setTimeout(function() {
+		document.getElementById("sub-backdrop").style.display = "block";
+	}, 7000);
+}
+*/
